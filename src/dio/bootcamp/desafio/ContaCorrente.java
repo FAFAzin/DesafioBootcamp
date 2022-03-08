3672b9a65532153de2c1b0e0ebb20f7b532e02f5
Josafajosina <josafajosina@gmail.com>
2022-03-08 00:02:45 -0300

    first commit

---
+++ src/dio/bootcamp/desafio/ContaCorrente.java
@@ -0,0 +1,41 @@
+package dio.bootcamp.desafio;
+
+public class ContaCorrente {
+
+    // atributos
+    private int agencia;
+    private int numero;
+    private int saldo;
+
+    //Getters - Não existe lógica para inserir os setters 
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+
+    public int getNumero() {
+        return numero;
+    }
+
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+
+    //métodos
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+
+}
---
+++ src/dio/bootcamp/desafio/ContaPoupança.java
@@ -0,0 +1,35 @@
+package dio.bootcamp.desafio;
+
+public class ContaPoupança {
+
+    //atributos
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    //getters and setters
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
